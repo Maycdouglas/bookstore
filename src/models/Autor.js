52f6dfd0336303add397467eaf977bd9f1e93f43
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2024-10-30 17:06:23 -0300

    Cria modelos Livro e Autor

---
+++ src/models/Autor.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {autorSchema} from "./Autor.js"
+
+const autorSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    nome: {type: String},
+    nacionalidade: {type: String}
+}, {versionKey: false})
+
+const autor = mongoose.model("autores", autorSchema)
+
+export {autor, autorSchema}
